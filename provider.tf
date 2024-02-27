3db1b886d2ba3066a50d719f32d3273f4dcc0e4a
assist.user <assist.user@754478843459>
2024-02-27 14:49:09 -0500

    a

--- provider.tf
+++ provider.tf
@@ -6,6 +6,10 @@ terraform {
   }
 }
 
-provider "ibm" {
-  ibmcloud_api_key   = var.ibmcloud_api_key
+provider "ibm" {    
+    ibmcloud_api_key = "PIVsewk3KK6jV8qXc_RUnuH4ObAjGPLAKwiq2oJ8HriD"
+    region = "us-south"
+
+    iaas_classic_username = "2112072_ggonzalez@assist.com.co"
+    iaas_classic_api_key = "28796ab616e0dcf6d85277f8775112ba32bf0db0469c3e5000a3bc8decd4ba73"
 }
